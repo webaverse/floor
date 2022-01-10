c7732b375f62a919dbe3f57bf1269751f079c4f3
Avaer Kazmer <a@modules.io>
2022-01-10 15:28:08 -0500

    Debugging physics

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import * as THREE from 'three';
 import metaversefile from 'metaversefile';
-const {useCleanup, usePhysics} = metaversefile;
+const {useApp, useCleanup, usePhysics} = metaversefile;
 
-export default () => {  
+export default e => {
+  const app = useApp();
   const physics = usePhysics();
+  
   const streetSize = 1000;
   const streetHeight = 1000;
   const floorPhysicsId = physics.addBoxGeometry(
     new THREE.Vector3(0, -streetHeight, 0),
     new THREE.Quaternion(),
-    new THREE.Vector3(streetSize * 0.5, streetHeight, streetSize * 0.5),
+    new THREE.Vector3(streetSize, 2000, streetSize).multiplyScalar(0.5),
     false
   );
   
   useCleanup(() => {
     physics.removeGeometry(floorPhysicsId);
   });
+
+  return app;
 }
